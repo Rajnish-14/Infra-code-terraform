6d9fe7cfb230fb9a08285b33880279fd5bfa677f
unknown <rajnish.s44@gmail.com>
2025-09-03 22:01:26 +0530

    rgadded

--- main.tf
+++ main.tf
@@ -1,1 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.42.0"
+    }
+  }
+}
 
+provider "azurerm" {
+  # Configuration options
+  features {}
+  subscription_id = "fc0f895b-de69-4d59-96a9-b13d6894a7e7"
+}
+
+resource "azurerm_resource_group" "github-rg"{
+  name     = "rg123"
+  location = "West Europe"
+}
