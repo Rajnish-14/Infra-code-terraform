2db66d1efcba18be0d9cfe717ecc76baad7855a0
Rajnish-14 <rajnish.s44@gmail.com>
2025-09-20 21:10:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ terraform {
 }
 terraform {
   backend "azurerm" {
+    resource_group_name   = "dev-rg1"
     storage_account_name = "abcd12349974"                                 # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "tfstate"                                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "prod.terraform.tfstate"                   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
